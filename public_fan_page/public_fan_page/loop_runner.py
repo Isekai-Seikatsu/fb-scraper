8b19df5e0d32028abe6a5f6bd2348e84e3a4b97f
isekai <brian1689878638@gmail.com>
2020-04-17 03:29:45 +0800

    Add ReactorsSpider to loop

--- public_fan_page/public_fan_page/loop_runner.py
+++ public_fan_page/public_fan_page/loop_runner.py
@@ -1,19 +1,27 @@
-from twisted.internet import reactor
 import scrapy
 from scrapy.crawler import CrawlerRunner
-from spiders.fan_page import FanPageSpider
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
+from twisted.internet import reactor
+
+from spiders.fan_page import FanPageSpider
+from spiders.reactors import ReactorsSpider
 
 
-def run_crawl():
-    deferred = runner.crawl(FanPageSpider)
-    deferred.addCallback(lambda _: reactor.callLater(60, run_crawl))
+def run_crawl(spider, interval=60):
+    deferred = runner.crawl(spider)
+    deferred.addCallback(
+        lambda _: reactor.callLater(
+            interval, lambda _: run_crawl(spider, interval))
+    )
     return deferred
 
+
 runner = CrawlerRunner(get_project_settings())
-configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
-run_crawl()
+configure_logging(settings={'LOG_LEVEL': 'INFO'})
+
+run_crawl(FanPageSpider)
+run_crawl(ReactorsSpider, 600)
 # d = runner.crawl(FanPageSpider)
 # d.addBoth(lambda _: reactor.stop())
 reactor.run()
