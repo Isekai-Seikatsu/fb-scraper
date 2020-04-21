e68418144b4d46899489171db8c45c321e7f258c
isekai <brian1689878638@gmail.com>
2020-04-21 09:27:35 +0800

    Update fan_page from fb_user

---
+++ public_fan_page/public_fan_page/update_loop.py
@@ -0,0 +1,50 @@
+import asyncio
+import os
+
+from motor.motor_asyncio import AsyncIOMotorClient
+from datetime import datetime
+
+client = AsyncIOMotorClient(os.getenv('SCRAPY_SETTING_MONGO_URI'))
+db = client.get_default_database()
+
+
+async def update_fan_page_from_fb_user():
+    cursor = db.fb_user.find(
+        {
+            'action_path': {
+                '$ne': "/a/mobile/friends/add_friend.php",
+                '$exists': 1
+            }
+        }
+    )
+
+    # TODO create_task
+    futures = [
+        db.fan_page.update_one(
+            {'page_id': user['uid']},
+            {
+                '$set': {
+                    'link': 'https://www.facebook.com' + user['profile_link']
+                }
+            },
+            upsert=True
+        )
+        async for user in cursor
+    ]
+
+    results = await asyncio.gather(*futures)
+    newline = '\n'
+    print(f"{newline.join([str(result.raw_result) for result in results])}\n{'='*20}\n{datetime.now()}\n\n")
+    return results
+
+async def main_loop():
+    print('main loop start\n')
+    while True:
+        await asyncio.gather(
+            update_fan_page_from_fb_user(),
+            asyncio.sleep(3600)
+        )
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main_loop())
