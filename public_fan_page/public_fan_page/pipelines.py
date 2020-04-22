9c895a4da81c2c9361a3c0f6ba07a3d50326792b
isekai <brian1689878638@gmail.com>
2020-04-23 07:23:37 +0800

    Asyncio pipeline!

--- public_fan_page/public_fan_page/pipelines.py
+++ public_fan_page/public_fan_page/pipelines.py
@@ -1,9 +1,10 @@
 import abc
+import asyncio
 import datetime
 
-import pymongo
 import pytz
-from pymongo.errors import OperationFailure, DuplicateKeyError
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.errors import DuplicateKeyError, OperationFailure
 from pymongo.results import UpdateResult
 
 
@@ -22,14 +23,8 @@ class MongoPipelineABC(abc.ABC):
         return cls(mongo_uri=crawler.settings.get('MONGO_URI'))
 
     def open_spider(self, spider):
-        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.client = AsyncIOMotorClient(self.mongo_uri)
         self.db = self.client.get_default_database()
-        try:
-            # Make sure that connection through URI is success
-            self.client.admin.command('ismaster')
-        except OperationFailure as opfail:
-            self.logger.error(opfail)
-            raise opfail
 
     def close_spider(self, spider):
         self.client.close()
@@ -42,7 +37,7 @@ class MongoPipelineABC(abc.ABC):
 class MongoFanPagePipeline(MongoPipelineABC):
     EMPTY_TIME = datetime.time()
 
-    def process_item(self, item, spider):
+    async def process_item(self, item, spider):
         utc_now = datetime.datetime.utcnow()
         utc_date = datetime.datetime.combine(utc_now, self.EMPTY_TIME)
         post_id = int(item['fbid'])
@@ -54,7 +49,7 @@ class MongoFanPagePipeline(MongoPipelineABC):
                                             for k, v in item['reactions'].items()]
 
         hist_coll = self.db.history
-        result: UpdateResult = hist_coll.post_reactions.update_one(
+        result_fu = hist_coll.post_reactions.update_one(
             {'post_id': post_id, 'date': utc_date},
             {
                 '$push': {
@@ -63,13 +58,12 @@ class MongoFanPagePipeline(MongoPipelineABC):
             },
             upsert=True
         )
-        spider.logger.info(result.raw_result)
 
         posted_time = (datetime.datetime
                        .fromtimestamp(int(item['posted_time']))
                        .astimezone(pytz.timezone(spider.settings.get('TIMEZONE'))))
 
-        resp = self.db.post.find_one_and_update(
+        resp_fu = self.db.post.find_one_and_update(
             {
                 'post_id': post_id,
                 'page_id': int(item['page_id']),
@@ -87,12 +81,17 @@ class MongoFanPagePipeline(MongoPipelineABC):
             upsert=True
         )
 
+        result: UpdateResult
+        result, resp = await asyncio.gather(result_fu, resp_fu)
+
+        spider.logger.info(result.raw_result)
+
         if resp is None:
             spider.logger.info('Post upsert happend')
         else:
             if resp['msg'] != item['msg']:
                 # update msg history
-                result: UpdateResult = hist_coll.post_msg.update_one(
+                result: UpdateResult = await hist_coll.post_msg.update_one(
                     {'post_id': post_id},
                     {
                         '$push': {
@@ -110,18 +109,21 @@ class MongoFanPagePipeline(MongoPipelineABC):
 
 
 class MongoPostReactorsPipeline(MongoPipelineABC):
-    def process_item(self, item, spider):
-
-        ids = [
+    async def process_item(self, item, spider):
+        ids_fus = [
             self.db.fb_user.find_one_and_update(
                 doc,
                 {'$currentDate': {'update_time': True}},
                 projection={'_id': 1},
                 return_document=True,
                 upsert=True
-            )['_id'] for doc in item['reactors']]
+            )
+            for doc in item['reactors']
+        ]
 
-        update_result = self.db.history.post_reactors.update_one(
+        ids = [doc['_id'] for doc in await asyncio.gather(*ids_fus)]
+
+        update_result = await self.db.history.post_reactors.update_one(
             {'post_id': item['post_id'], 'date': item['start_date']},
             {
                 '$push': {
