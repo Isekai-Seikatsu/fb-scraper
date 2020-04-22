76c0aa5b388057505aae457352c16af19675a32d
isekai <brian1689878638@gmail.com>
2020-04-23 07:22:32 +0800

    Move install_reactor right after import cause twisted AsyncioSelectorReactor issue

--- public_fan_page/public_fan_page/loop_runner.py
+++ public_fan_page/public_fan_page/loop_runner.py
@@ -4,11 +4,15 @@ import scrapy
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
+
 from scrapy.utils.reactor import install_reactor
+install_reactor('twisted.internet.asyncioreactor.AsyncioSelectorReactor')
+
 from twisted.internet import reactor
 
 from spiders.fan_page import FanPageSpider
 from spiders.reactors import ReactorsSpider
+
 
 
 def run_crawl(spider, interval=60):
@@ -19,8 +23,6 @@ def run_crawl(spider, interval=60):
     )
     return deferred
 
-
-install_reactor('twisted.internet.asyncioreactor.AsyncioSelectorReactor')
 
 runner = CrawlerRunner(get_project_settings())
 configure_logging(settings={'LOG_LEVEL': 'INFO'})
